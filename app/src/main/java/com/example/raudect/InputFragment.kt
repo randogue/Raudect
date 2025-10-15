5fd08d9cd7c4cf3b566a47d867e230d5716336f1
Herqz <pilrealthegunner@gmail.com>
2025-10-15 19:50:59 +0700

    Input into Database
    input from inputFragment is now able to put data into the database

--- app/src/main/java/com/example/raudect/InputFragment.kt
+++ app/src/main/java/com/example/raudect/InputFragment.kt
@@ -2,13 +2,21 @@ package com.example.raudect
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlin.random.Random
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +34,12 @@ class InputFragment : Fragment() {
     private var param2: String? = null
 
     private var themedContext: Context? = null
+
+
+    //db & auth init
+    private lateinit var fraudTestRef: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,11 +65,71 @@ class InputFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        //db & auth instantiation
+        fraudTestRef = FirebaseDatabase.getInstance().getReference("fraud_test_info")
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+
+        //input view holding
+        //personal
+        val pCardNumber = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_cardNumber_layout)
+        val pDateOfBirth = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_dateOfBirth_layout)
+        val pJob = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_job_layout)
+        val pAddress = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_address_layout)
+        val pCityPopulation = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_cityPopulation_layout)
+        //transaction
+        val tTime = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_transactionTime_layout)
+        val tCategory = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_category_layout)
+        val tAmount = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_transactionAmount_layout)
+        val tLatitude = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_transactionLatitude_layout)
+        val tLongitude = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_transactionLongitude_layout)
+        val tMerchant = view.findViewById<TextInputEditText>(R.id.inputFragment_inputEdit_merchantsName_layout)
+
+
         view.findViewById<Button>(R.id.inputFragment_button_submit).
                 setOnClickListener {
-                    findNavController().navigate(
-                        R.id.action_inputFragment_listFragment
+                    //data mapping
+                    val newRowRef = fraudTestRef.push()
+                    val testId = newRowRef.key
+                    val rowData = mapOf(
+                        "uid" to user?.uid,
+                        "tid" to testId,
+                        //personal
+                        "cardnum" to pCardNumber.text?.toString()?.takeIf{it.isNotBlank()},
+                        "dateofbirth" to pDateOfBirth.text?.toString()?.takeIf{it.isNotBlank()},
+                        "job" to pJob.text?.toString()?.takeIf{it.isNotBlank()},
+                        "address" to pAddress.text?.toString()?.takeIf{it.isNotBlank()},
+                        "citypop" to pCityPopulation.text?.toString()?.toFloatOrNull(),
+                        //transaction
+                        "date" to tTime.text?.toString()?.takeIf{it.isNotBlank()},
+                        "category" to tCategory.text?.toString()?.takeIf{it.isNotBlank()},
+                        "amount" to tAmount.text?.toString()?.toFloatOrNull(),
+                        "lat" to tLatitude.text?.toString()?.toFloatOrNull(),
+                        "lon" to tLongitude.text?.toString()?.toFloatOrNull(),
+                        "merchant" to tMerchant.text?.toString()?.takeIf{it.isNotBlank()},
+
+                        //result manipulation
+                        "isfraud" to Random.nextBoolean()
                     )
+
+                    //setting value
+                    newRowRef.setValue(rowData).addOnCompleteListener { task->
+                        if(task.isSuccessful){
+                            Log.d("INPUT_FRAGMENT","Write success!")
+                            Toast.makeText(context, "Successfully added data!", Toast.LENGTH_SHORT).show()
+                            findNavController().navigate(
+                                R.id.action_inputFragment_listFragment
+                            )
+                        }
+                        else{
+                            Log.e("INPUT_FRAGMENT", "Write failed: ${task.exception?.message}")
+                            Toast.makeText(context, "Failed to add data...", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
                 }
     }
 
